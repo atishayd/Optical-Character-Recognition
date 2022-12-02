accddec32a4a1489ab43d2642a30dd54d29d60b1
atishayd <atishayd@usc.edu>
2022-12-02 15:39:31 -0800

    Update bmplib.h

--- project4-ocr/bmplib.h
+++ project4-ocr/bmplib.h
@@ -7,18 +7,13 @@
 const int SIZE = 256;
 const int RGB = 3;
 // Note: the read-write functions return 0 on success, 1 on error.
-
-// USE THESE FOR PR4
-//================================
 uint8_t** readGSBMP(const char filename[], int& height, int& width);
 int writeGSBMP(const char filename[], uint8_t **outputImage,int height,int width);
 
 
 
 // THESE ONLY WORK FOR 256x256
-// read grayscale image from the file specified by filename, into inputImage
 int readGSBMP(const char filename[], uint8_t image[][SIZE]);
-
 
 // write grayscale image to the file specified by filename, from outputImage
 int writeGSBMP(const char filename[], uint8_t outputImage[][SIZE]);
@@ -33,8 +28,5 @@ uint8_t ***readRGBBMP(const char filename[], int& height,int& width);
 int writeRGBBMP(const char filename[], uint8_t outputImage[][SIZE][RGB]);
 
 int writeRGBBMP(const char filename[], uint8_t ***outputImage,int height,int width);
-
-
-
 
 #endif
